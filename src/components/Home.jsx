9e14bc688c2e64a2be54b6f2044984bbc8c264be
Diksha subedi <subedidikshya321@gmail.com>
2022-11-28 16:39:51 +0545

    dom manipulation

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import dikshaa from "../images/dikshaa.jpg";
+import "./Home.css";
+
+const Home = () => {
+   
+
+
+
+
+
+  return (
+    <>
+    <h1 className="heading">Diksha</h1>
+    <div className='img_div'>
+    
+      <img src={dikshaa} alt="diksha" />
+      <img src={dikshaa} alt="diksha" />
+      <img src={dikshaa} alt="diksha" />
+    
+    
+    </div>
+    </>
+        
+   
+  );
+};
+
+export default Home;
