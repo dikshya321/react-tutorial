85992ab36561aee9d28e0c000683893663d90e77
Diksha subedi <subedidikshya321@gmail.com>
2022-12-04 16:31:16 +0545

    props

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,15 +1,76 @@
 import React from 'react'
+import {useState} from 'react';
 
-const Home = () => {
+const Home = () => {//controlled component
+  const[Value,setValue]=useState("")
   return (
     <>
-    <div>
-      <h1>MY sweet Home</h1>
-      </div>
-    
-    
+    <div className="myHome">
+      <input type="text"placeholder="enter your email" 
+      onChange={(e)=> setValue(e.target.value)}/>
+      <br />
+      <h1>Value:{Value}</h1>
+    </div>
     </>
-  )
-}
+  );
+};
+
+
+// const Counter = ()=>{
+//  const[count,setCount]=useState(0);
+//   return(
+//     <>
+//    <div>
+//    <h1>{count}</h1>
+//     <button onClick={()=>setCount(count+1)}>Click Me</button>
+//    </div>
+    
+//     </>
+//   );
+// };
+
+// const RedHOC=(props)=>{
+//   return(
+//     <>
+//     <div>
+
+// <h1 style ={{backgroundColor:"red",width:"100px"}}>Red<props.cmp/></h1>
+
+//     </div>
+    
+    
+//     </>
+//   )
+// }
+
+// const GreenHOC=(props)=>{
+//   return(
+//     <>
+//     <div>
+
+// <h1 style ={{backgroundColor:"green",width:"100px"}}>Green<props.cmp/></h1>
+
+//     </div>
+    
+    
+//     </>
+//   );
+// };
+
+// const BlueHOC=(props)=>{
+//   return(
+//     <>
+//     <div>
+
+// <h1 style ={{backgroundColor:"blue",width:"100px"}}>Blue<props.cmp/></h1>
+
+//     </div>
+    
+    
+//     </>
+//   )
+// }
+
+
 
 export default Home;
