991d69f19b6a20f28e7b2f6fa5f229c44b3bef8c
Diksha subedi <subedidikshya321@gmail.com>
2022-12-16 16:20:23 +0545

    local and session storage

--- src/components/pages/user/Register.jsx
+++ src/components/pages/user/Register.jsx
@@ -1,44 +1,157 @@
-import React from 'react';
-// import { useEffect } from 'react';
-import { useState } from 'react';
-import Login from './Login';
-const Register = () => {
-//  const [data,setData]= useState(0)   
-//     const handleSubmit=()=>{
-//         setData (data+1) ;
-//         alert(data);
+// import React, { useRef, useState ,useEffect} from "react";
+// import axios from "axios";
+// // //import Login from './Login';
 
-//     };
-//     console.warn("_______________");
+// // // const Register = () => {
+// // //   const [count, setCount] = useState(0);
+// // //   const [data, setData] = useState(10);
+
+// // //   // const myMemo = useMemo(() => {
+// // //   //   return count * 5;
+// // //   // }, [count]);
+
+// // //   // const mmyMemo = useMemo(() => {
+// // //   //   return count / 2;
+// // //   // }, [count]);
+// // //   // return (
+// // //     <>
+// // //       <div>
+// // //         {/* <Login count={count}/> */}
+// // //         <h1>Count:{count}</h1>
+// // //         <h1>Data:{data}</h1>
+// // //         <p>{myMemo}</p>
+// // //         <p>{mmyMemo}</p>
+// // //         <button onClick={() => setCount(count + 1)}>Update Count</button>
+// // //         <button onClick={() => setData(data * 5)}>Update Data</button>
+// // //       </div>
+// // //     </>
+// // //   );
+// // // };
+
+
+// // const Register = () => {
+// //   const [show,setShow]=useState(false);
+// //   const inputRef= useRef(null);
+
+// //   const handleClick=(event)=>{
+// //   event.preventDefault();
+
+// //   const name=inputRef.current.value;
+// //   name === ""? alert("please fill the form"): setShow(true);
+// //   };
+// //   return (
+// //     <>
+// //     <div>
+// //       <label>Enter your data</label>
+// //       <br/>
+// //       <input type="text" ref={inputRef}/>
+// //       <br/>
+// //       <button onClick={handleClick}>Click Me</button>
+// //       <br/>
+// //       <p>{show? your lucky number is :${inputRef.current.value}:" "}</p>
+// //     </div>
+    
+// //     </>
+// //   )
+// // }
+
+// // export default Register;
+
+// // import React, { useEffect, useState } from 'react';
+
+// // const Register = () => {
+// //   const [count,setCount]=useState(0);
+
+// //   useEffect(()=>{
+// //     console.log("useEffect Runs")
+// //     const interval=setInterval(() => {
+// //      setCount((prev)=>prev+1); 
+// //     }, 1000);
+
+// //     return ()=>{
+// //       clearInterval(interval);
+// //       console.log("cleaning the effect");
+// //     }
+// //  },[]);
+
+// // return(
+  
+
+// //     <>
+// //     <div>
+// //       <h1>Count:{count}cleaning return function </h1>
+// //     </div>
+    
+// //     </>
+// //   )
+// // }
+
+// // export default Register
+
+// import React, { useCallback, useState } from 'react';
+// import { useEffect } from 'react';
+// import Login from "./Login";
+
+// const Register = () => {
+//     useEffect(()=>{
+// const res = axios.get("https://jsonplaceholder.typicode.com/posts"));
+// .then(()=>console.log(res.data)
+// .catch((error)=>console.log(error));
+//     },[])
+// //   const [count,setCount]=useState(0);
+// //   const [todos,setTodo]=useState([]);
+// //   const handleIncrement=()=>{
+// //     setCount(count+1);
+// //   }
+// //   const AddTodo=useCallback(
+// //     () => {
+// //       setTodo((prev)=>[...prev,'new Entry'])
+// //     },[todos]);
+  
+
 //   return (
 //     <>
+//     {/* <div>
+//       <Login todos={todos} AddTodo={AddTodo}/>
+//       <h1>{count}</h1>
+//       <button onClick={handleIncrement}>+</button>
+//     </div> */}
     
-//     <div>
+//     </>
+//   )
+// }
 
-// <h1>{data}</h1>
-// <button onClick={handleSubmit}>Clickme</button>
-//     </div>
+// export default Register;
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
 
-const [count,setCount]=useState(0);
-const[data,setData]=useState(0);
-// useEffect(()=>{
-//   alert(`count number is:${count}`)
-// },[count,data]);
-    return(
-<>
- <div>
-  <h3>Count:{count}</h3><br/>
-  <h3>Data:{data}</h3>
-  <Login count={count}data={data}/>
-  <button onClick={()=>setCount(count+1)}>Update Count</button>
-  <button onClick={()=>setData(data+1)}>Update Data</button>
- </div>
+const Register = () => {
+  const [myData,setMyData]=useState([]);
+  useEffect(()=>{
+    axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then((res)=>setMyData(res.data))
+    .catch((error)=>console.log(error));
 
-</>
-    )
 
-    
-  
+  },[])
+  return (
+    <>
+    <div>
+
+      {myData.map((curValue)=>{
+        return(
+          <div className='card' key={curValue.id}>
+            <h1>{curValue.title}</h1>
+            <p>{curValue.body}</p>
+          </div>
+        )
+      })
+
+      }
+    </div>
+    </>
+  )
 }
 
 export default Register
